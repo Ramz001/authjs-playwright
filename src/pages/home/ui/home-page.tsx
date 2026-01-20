6d6969a6da95b60ddf3978a401cdb50d813220be
Ramz001 <rkenjaev1@gmail.com>
2026-01-20 07:04:48 +0500

    style: update layout and home page for improved readability and consistency

--- src/pages/home/ui/home-page.tsx
+++ src/pages/home/ui/home-page.tsx
@@ -1,14 +1,16 @@
-import { Button } from '@/shared/ui/button'
-import Link from 'next/link'
+import { Button } from "@/shared/ui/button";
+import Link from "next/link";
 
 export default function HomePage() {
   return (
     <div className="flex h-screen w-screen flex-col items-center justify-center gap-4">
-      <h1 className="mb-2 text-4xl font-bold text-gray-800">Hey there! 👋</h1>
-      <h2 className="mb-6 text-xl text-gray-600">Welcome to our Auth Demo</h2>
+      <h1 className="mb-2 text-4xl font-bold text-foreground">Hey there! 👋</h1>
+      <h2 className="mb-6 text-xl text-muted-foreground">
+        Welcome to our Auth Demo
+      </h2>
       <Button>
-        <Link href={'/auth/sign-in'}>Get Started with Authentication</Link>
+        <Link href={"/auth/sign-in"}>Get Started with Authentication</Link>
       </Button>
     </div>
-  )
+  );
 }
