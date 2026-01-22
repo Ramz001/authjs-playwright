402e000a753a7bc7987bbe50237652f6d6cd7344
Ramz001 <rkenjaev1@gmail.com>
2026-01-22 18:31:10 +0500

    fix(tests): update route-guard tests to use relative URLs

--- tests/auth/route-guard.spec.ts
+++ tests/auth/route-guard.spec.ts
@@ -2,14 +2,14 @@ import { test, expect } from '@playwright/test'
 
 test.describe('unauthenticated user', () => {
   test('can access the home page', async ({ page }) => {
-    await page.goto('http://localhost:3000/')
+    await page.goto('/')
     await expect(
       page.getByRole('heading', { name: /Welcome to our Auth Demo/ })
     ).toBeVisible()
   })
 
   test('can access the settings page', async ({ page }) => {
-    await page.goto(`http://localhost:3000/settings`)
-    await expect(page).toHaveURL('http://localhost:3000/auth/sign-in')
+    await page.goto(`/settings`)
+    await expect(page).toHaveURL('/auth/sign-in')
   })
 })
