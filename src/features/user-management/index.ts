c1c4be51a77112fe7d4ed62b4241f30f02f99564
Ramz001 <rkenjaev1@gmail.com>
2026-01-22 17:47:02 +0500

    feat: remove user management feature and related components

--- src/features/user-management/index.ts
+++
@@ -1,4 +0,0 @@
-// UI Components
-export { SessionManagementCard } from './ui/session-management-card'
-export { UsersListCard } from './ui/users-list-card'
-export { UserDetailsPanel } from './ui/user-details-panel'
--- src/pages/admin/admin.page.tsx
+++ src/pages/admin/admin.page.tsx
@@ -1,12 +1,9 @@
 import Gutter from '@shared/ui/gutter'
-import { UserManagement } from '@widgets/user-management'
 
 export default async function AdminPage() {
   return (
     <Gutter>
       <h1 className="my-8 text-3xl font-bold">Admin</h1>
-
-      <UserManagement />
     </Gutter>
   )
 }
