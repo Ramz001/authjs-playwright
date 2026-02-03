35033f121fe147d0fd5d6876c4b204901fbfa8bf
Ramz001 <rkenjaev1@gmail.com>
2026-02-04 00:01:27 +0500

    feat: reorganize global styles and update layout to improve structure and maintainability

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,9 @@
 import type { Metadata } from 'next'
-import { Geist, Geist_Mono, DM_Sans } from 'next/font/google'
-import './globals.css'
+import { DM_Sans } from 'next/font/google'
+import '@app/styles/globals.css'
 import RootProvider from '@app/providers/root.provider'
 
 const dmSans = DM_Sans({ subsets: ['latin'], variable: '--font-sans' })
-
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-})
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-})
 
 export const metadata: Metadata = {
   title: 'Auth.js Playwright Test',
@@ -26,10 +16,8 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en" className={dmSans.variable} suppressHydrationWarning>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${dmSans.variable} antialiased`}>
         <RootProvider>{children}</RootProvider>
       </body>
     </html>
