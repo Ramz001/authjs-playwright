4f8cdda6bf1e7ebbbfa3920183713b7eb9936926
Ramz001 <rkenjaev1@gmail.com>
2026-02-20 23:55:55 +0500

    feat(background): move BackgroundPattern to provider and update import path

--- src/app/providers/root.provider.tsx
+++ src/app/providers/root.provider.tsx
@@ -1,7 +1,7 @@
 import ToastProvider from './toast.provider'
 import AuthSessionProvider from './session.provider'
 import ThemeProvider from './theme.provider'
-import BackgroundPattern from '@app/styles/background-pattern'
+import BackgroundPattern from './background-pattern.provider'
 
 export default function RootProvider({
   children,
